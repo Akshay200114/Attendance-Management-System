8845ffffaaa4d7b40cbfc4ee64c8997b4d10bb2c
Striker2k <hrushizagade2k@gmail.com>
2020-11-28 23:39:31 +0530

    Check static files

--- qr/models.py
+++ qr/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    is_teacher = models.BooleanField(null=True)
 # Create your models here.
--- qr/views.py
+++ qr/views.py
@@ -1,6 +1,58 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+from django.urls import reverse
+
 
 # Create your views here.
-def login(request):
-    return render(request,'qr\login.html')
+
+@login_required
+def index(request):
+    return render(request, 'qr\index.html')
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"] 
+        password = request.POST["password"] 
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "qr/login.html", {
+                "message": "Invalid username and/or password."
+                })
+    return render(request, "qr/login.html")
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        is_teacher=False
+        if "is_teacher" in request.POST.keys():
+            is_teacher = request.POST["is_teacher"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "qr/register.html",  {
+                "message": "Passwords must match."
+                })
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password, is_teacher=is_teacher)
+            user.save()
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        except IntegrityError:
+            return render(request, "qr/register.html", {
+                "message": "Username already taken."
+                })
+    return render(request, "qr/register.html")
